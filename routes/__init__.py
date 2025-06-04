bf4bed307d476200ca3e87bce2fff2b9cbe1e1cd
Walter-Hartwell-White <jinu2908@yahoo.com>
2025-06-04 15:29:34 +0530

    Structured Folders...

--- routes/__init__.py
+++
@@ -1,9 +0,0 @@
-from __main__ import app, db
-from models.categories_models import Category
-from models.colors_models import Color
-from models.images_models import Image
-from models.products_models import Product
-from models.reviews_models import Review
-from models.subcategories_models import Subcategory
-from models.users_models import User
-from models.wishlists_models import Wishlist
