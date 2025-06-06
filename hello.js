c8eb6dadbc53465dcec86203ce4155e7dd292ae8
hemant0816 <preashhemant@gmail.com>
2025-06-06 14:30:08 +0530

    deleteting all file

--- hello.js
+++
@@ -1,1 +0,0 @@
-console.log("hekll its an testing command")
--- main.js
+++
@@ -1,6 +0,0 @@
-function login(e) {
-  e.preventDefault();
-  document.getElementById("loginPage").style.display = "none";
-  document.getElementById("mainSite").style.display = "block";
-  return false;
-}
