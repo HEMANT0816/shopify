56be0aefe4fe026cc02ddbe1529160575a602c7f
Khushii <khushirani2511@gmail.com>
2025-06-06 12:34:55 +0530

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,52 +1,43 @@
-// main.jsx
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import App from './App'
-
-import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Product from "./pages/Product";
-import ProductDetail from "./pages/Productdetail";
-import Cart from "./pages/Cart";
-import Checkout from "./pages/Checkout";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Orders from "./pages/Orders";
-import UserProfile from "./pages/Profile";
-import SearchHistory from "./pages/History";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-
-export default function AppRoutes() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/products" element={<AllProducts />} />
-      <Route path="/products/:id" element={<ProductDetail />} />
-      <Route path="/cart" element={<Cart />} />
-      <Route path="/checkout" element={<Checkout />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/orders" element={<Orders />} />
-      <Route path="/profile" element={<UserProfile />} />
-      <Route path="/search-history" element={<SearchHistory />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
-    </Routes>
-  );
-}
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import App from './App';
+import Home from './Home';
+import Product from './pages/Product';
+import ProductDetail from './pages/Productdetail';
+import Cart from './pages/Cart';
+import Checkout from './pages/Checkout';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Orders from './pages/Orders';
+import UserProfile from './pages/Profile';
+import SearchHistory from './pages/History';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <App />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/products', element: <Product /> },
+      { path: '/products/:id', element: <ProductDetail /> },
+      { path: '/cart', element: <Cart /> },
+      { path: '/checkout', element: <Checkout /> },
+      { path: '/login', element: <Login /> },
+      { path: '/register', element: <Register /> },
+      { path: '/orders', element: <Orders /> },
+      { path: '/profile', element: <UserProfile /> },
+      { path: '/search-history', element: <SearchHistory /> },
+      { path: '/about', element: <About /> },
+      { path: '/contact', element: <Contact /> },
+    ],
   },
-])
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
