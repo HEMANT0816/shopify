bedd6ba012c05e11ac1b29d2c1e3e0ab39eda6cb
Khushiirani25 <khushirani2511@gmail.com>
2025-06-06 14:07:05 +0530

    lshdjs

---
+++ main.js
@@ -0,0 +1,6 @@
+function login(e) {
+  e.preventDefault();
+  document.getElementById("loginPage").style.display = "none";
+  document.getElementById("mainSite").style.display = "block";
+  return false;
+}
